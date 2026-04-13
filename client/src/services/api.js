9bf354919576dbe5683afbaa1625311435801871
Shoeb khan <139688250+Shoeb-code@users.noreply.github.com>
2026-04-13 20:08:03 +0530

    change the backend url to render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://fast-ai-interview-platform-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
